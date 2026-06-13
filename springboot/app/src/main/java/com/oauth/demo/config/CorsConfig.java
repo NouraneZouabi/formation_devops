cd5881e55850f562d967eaf7b5751e0b4f5eb6b2
nouranzouabi <nouranzouabi99@gmail.com>
2026-06-13 10:46:42 +0200

    changement des @IP

--- springboot/app/src/main/java/com/oauth/demo/config/CorsConfig.java
+++ springboot/app/src/main/java/com/oauth/demo/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://54.196.35.185:4200") 
+                .allowedOrigins("http://192.168.187.19:4200") 
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }

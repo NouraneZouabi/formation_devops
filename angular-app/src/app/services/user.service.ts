b97903f84332df5e75b4b9f0dec2c0ddf103e083
Nourane Zouabi <100202617+NouraneZouabi@users.noreply.github.com>
2026-07-05 07:25:14 +0100

    Update baseURL in UserService to new endpoint

--- angular-app/src/app/services/user.service.ts
+++ angular-app/src/app/services/user.service.ts
@@ -8,7 +8,7 @@ import { User } from '../models/User';
 })
 export class UserService {
 
-  baseURL:string="http://192.168.187.19:30456/user"
+  baseURL:string="http://18.206.77.148:30456/user"
   constructor(private  httpClient:HttpClient) { }
 
   getAllUsers():Observable<any>{
